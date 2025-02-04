00483c29ccd40648bd9214aae1627a0bba75fd45
Jesus Garland <33094958+jgarlandh@users.noreply.github.com>
2025-02-04 00:14:41 +0000

    fix/app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  generarCarta();
   console.log("Hello Rigo from the console!");
 };
+
+// Sintesis:
+// Numeros Arreglo para Palos de Naipe:
+// 1 = Pica
+// 2 = Corazon
+// 3 = Trébol
+// 4 = Diamante
+// ----------------------------------------
+// Numeros Arreglo para Numero de Naipe:
+// 1= Ace
+// 2-10 = Sin cambio
+// 11= Jack
+// 12= Queen
+// 13= King 
+// ----------------------------------------
+function generarCarta() {
+  let paloNaipe = ["♦","♥","♠","♣"];
+  let numeroNaipe = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+
+  let paloNaipeRandom = paloNaipe[Math.floor(Math.random() * paloNaipe.length)];
+  let numeroNaipeRandom = numeroNaipe[Math.floor(Math.random() * numeroNaipe.length)];
+  console.log(paloNaipeRandom, numeroNaipeRandom);
+  
+  document.getElementById("palo1").innerHTML = paloNaipeRandom;
+  document.getElementById("numeroCarta").innerHTML = numeroNaipeRandom;
+}
+
+
